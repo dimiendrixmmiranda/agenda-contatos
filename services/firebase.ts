756b0c6a8d373840ba185cbe4c8dde5ff52a4464
Dimi Martins <dimiendrixmmiranda@gmail.com>
2024-08-11 14:30:59 -0300

    update: correção nas informações de segurança

--- services/firebase.ts
+++ services/firebase.ts
@@ -4,13 +4,14 @@ import { getDatabase } from 'firebase/database';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDwTDULflBQXpsjVtjgn6TQxhMedVxSPsg",
-    authDomain: "agenda-contatos-480b9.firebaseapp.com",
-    projectId: "agenda-contatos-480b9",
-    storageBucket: "agenda-contatos-480b9.appspot.com",
-    messagingSenderId: "337621453857",
-    appId: "1:337621453857:web:defe516f53e5a49d31a760"
-};
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
